a4a1fced2402802d4b3763a27a76c7b114090773
DESKTOP-K966CTP\User <arturs.banga8@gmail.com>
2021-01-10 00:22:43 +0200

    Refactor, split up views into tasks and lists

--- app.py
+++ app.py
@@ -11,18 +11,18 @@ dataBaseObj = DataBase()
 @app.route('/')
 def hello_world():
     data = dataBaseObj.getAllTasks()
-    return render_template('index.html', data=data)
+    return render_template('tasks/index.html', data=data)
 
 
 @app.route('/add', methods=["GET", "POST"])
 def add_task():
     if request.method == "GET":
-        return render_template('add.html')
+        return render_template('tasks/add.html')
     else:
         description = request.form.get("taskDescription")
         dataBaseObj.setTask(description)
         data = dataBaseObj.getAllTasks()
-        return render_template('index.html', data=data)
+        return render_template('tasks/index.html', data=data)
 
 
 @app.route('/delete', methods=["GET"])
@@ -40,7 +40,7 @@ def edit():
         taskId = int(request.args.get("id"))
         task = dataBaseObj.getTaskById(taskId)[0]
         previous = task["description"]
-        return render_template('edit.html', previous=previous, taskId=taskId)
+        return render_template('tasks/edit.html', previous=previous, taskId=taskId)
     else:
         text = str(request.form.get("editedDesc"))
         taskId = int(request.form.get("id"))
