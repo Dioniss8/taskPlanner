2c5cc18999354e256382290eee414635a4414972
DESKTOP-K966CTP\User <arturs.banga8@gmail.com>
2021-01-15 00:34:50 +0200

    Small ideas

--- src/api/BaseNbaService.py
+++ src/api/BaseNbaService.py
@@ -1,11 +1,13 @@
 import http.client
+from flask import session
+from src.services.LoggingService import LoggingService
 
 
 class BaseNbaService:
 
     def __init__(self):
         self.conn = http.client.HTTPSConnection("api-nba-v1.rapidapi.com")
-
+        self.loggingService = LoggingService()
         self.headers = {
             'x-rapidapi-key': "68a7808744msh45159e559d39863p1f1216jsn53dde588f2c0",
             'x-rapidapi-host': "api-nba-v1.p.rapidapi.com"
@@ -14,7 +16,8 @@ class BaseNbaService:
     def getPlayersByLastName(self, lastName):
         url = "/players/lastname" + str(lastName)
         data = self.getAndFormatResponseAsString("GET", url)
-
+        userId = session["user_id"]
+        '''TODO'''
         return data
 
     def getAndFormatResponseAsString(self, requestType, url):
