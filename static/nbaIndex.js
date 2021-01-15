572de981c19ddb4c15e21eb768e1c422c99b1dc0
DESKTOP-K966CTP\User <arturs.banga8@gmail.com>
2021-01-15 21:18:44 +0200

    Proof that my api works

--- static/nbaIndex.js
+++ static/nbaIndex.js
@@ -2,20 +2,24 @@
 
 function getAllUsersFromDb()
 {
-    let data = null;
     let name = "get-all-users";
     console.log("SMTh");
     $.ajax({
         url: "/api/" + name
     }).done(function (res) {
-        appendToParent(res)
+        for (let i = 0; i<res.data.length; i++) {
+            let text = res.data[i].username + " " + res.data[i].id + "\n";
+            console.log(res.data[i]);
+            appendChildElementToTarget(text);
+        }
+        console.log(res.data.length);
     })
 }
 
-function appendToParent(text)
+function appendChildElementToTarget(text)
 {
-    let input = document.createElement("p");
-    input.innerHTML = text;
+    let base = document.createElement("div");
+    base.innerHTML = text;
     let parent = document.getElementById("target");
-    parent.appendChild(input);
+    parent.appendChild(base);
 }
