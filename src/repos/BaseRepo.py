1c7aa537b11654c3216b15d0f8304100ac8b79e2
DESKTOP-K966CTP\User <arturs.banga8@gmail.com>
2021-02-01 10:40:28 +0200

    Added an .env file, just for Db now yet

--- src/repos/BaseRepo.py
+++ src/repos/BaseRepo.py
@@ -1,7 +1,8 @@
+import os
 from cs50 import SQL
 
 
 class BaseRepo:
 
     def __init__(self):
-        self.db = SQL("sqlite:///identifier.sqlite")
+        self.db = SQL(os.environ.get("DATABASE_URL"))
